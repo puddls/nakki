07da25e6053f0df50ebd12ff68b7549ef9110fb7
Liam DeVoe <orionldevoe@gmail.com>
2020-03-09 11:01:33 -0400

    move controllers to single file

--- systemctl/linux_controller.py
+++
@@ -1,6 +0,0 @@
-class LinuxController:
-    def __init__(self):
-        print('linux')
-
-    def close(self, application):
-        print('closing {} on linux'.format(str(application)))
--- systemctl/windows_controller.py
+++
@@ -1,6 +0,0 @@
-class WindowsController:
-    def __init__(self):
-        print('windows')
-    
-    def close(self, application):
-        print('closing {} on windows'.format(str(application)))
