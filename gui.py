cd63d21aece5614e9c963d9c121f2aed490d3e12
Kate Meuse <katemeuse@gmail.com>
2020-03-11 10:50:04 -0400

    buttons now add basic widgets to creation zone

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QGridLayout, QLabel, QPushButton, QListWidget, QListWidgetItem, QLineEdit, QComboBox, QTabWidget, QScrollArea
 from PyQt5.QtGui import QPalette, QColor
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 
 class MainWindow(QFrame):
 
@@ -15,6 +15,7 @@ class MainWindow(QFrame):
         self.tabs.addTab(settings, "Settings")
         # Need to implement task list class
 
+
         self.layout = QGridLayout()
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
@@ -26,17 +27,21 @@ class CreateTaskPage(QFrame):
 
         self.layout = QGridLayout()
 
-        buttons = ConstructionButtons()
+        self.buttons = ConstructionButtons()
         name_box = QLineEdit();
         name_box.setPlaceholderText("Enter the name of your task here.")
 
         save = QPushButton("Save")
         save.clicked.connect(self.save_task)
 
-        create_box = CreationZone()
+        self.create_box = CreationZone()
 
-        self.layout.addWidget(buttons)
-        self.layout.addWidget(create_box)
+        self.buttons.action_create.clicked.connect(self.create_action_block)
+        self.buttons.object_create.clicked.connect(self.create_object_block)
+        self.buttons.cond_create.clicked.connect(self.create_cond_block)
+
+        self.layout.addWidget(self.buttons)
+        self.layout.addWidget(self.create_box)
         self.layout.addWidget(name_box)
         self.layout.addWidget(save)
 
@@ -45,6 +50,18 @@ class CreateTaskPage(QFrame):
     def save_task(self):
         print("no work")
 
+    def create_action_block(self):
+        new = ActionWidget(self.buttons.action_drop.currentData())
+        self.create_box.layout.addWidget(new)
+
+    def create_object_block(self):
+        new = ObjectWidget(self.buttons.object_drop.currentData())
+        self.create_box.layout.addWidget(new)
+
+    def create_cond_block(self):
+        new = CondWidget(self.buttons.cond_drop.currentData())
+        self.create_box.layout.addWidget(new)
+
 class ConstructionButtons(QFrame):
     def __init__(self):
         super().__init__()
@@ -52,43 +69,31 @@ class ConstructionButtons(QFrame):
         self.layout = QGridLayout()
 
         self.action_drop = QComboBox()
-        self.action_drop.addItem("Close")
-        self.action_drop.addItem("Open")
-        action_create = QPushButton("+")
+        self.action_drop.addItem("Close", "Close")
+        self.action_drop.addItem("Open", "Open")
+        self.action_create = QPushButton("+")
 
         self.object_drop = QComboBox()
-        self.object_drop.addItem("Discord")
-        self.object_drop.addItem("Spotify")
-        object_create = QPushButton("+")
+        self.object_drop.addItem("Discord", "Discord")
+        self.object_drop.addItem("Spotify", "Spotify")
+        self.object_create = QPushButton("+")
 
         self.cond_drop = QComboBox()
-        self.cond_drop.addItem("9 AM")
-        self.cond_drop.addItem("2 PM")
-        cond_create = QPushButton("+")
-
-        action_create.clicked.connect(self.create_action_block)
-        object_create.clicked.connect(self.create_object_block)
-        cond_create.clicked.connect(self.create_cond_block)
+        self.cond_drop.addItem("9 AM", "9 AM")
+        self.cond_drop.addItem("2 PM", "2 PM")
+        self.cond_create = QPushButton("+")
 
         self.layout.addWidget(self.action_drop, 1, 1, 1, 3)
-        self.layout.addWidget(action_create, 1, 4, 1, 1)
+        self.layout.addWidget(self.action_create, 1, 4, 1, 1)
 
         self.layout.addWidget(self.object_drop, 1, 5, 1, 3)
-        self.layout.addWidget(object_create, 1, 8, 1, 1)
+        self.layout.addWidget(self.object_create, 1, 8, 1, 1)
 
         self.layout.addWidget(self.cond_drop, 1, 9, 1, 3)
-        self.layout.addWidget(cond_create, 1, 12, 1, 1)
+        self.layout.addWidget(self.cond_create, 1, 12, 1, 1)
 
         self.setLayout(self.layout)
 
-    def create_action_block(self):
-        print("no work")
-
-    def create_object_block(self):
-        print("no work")
-
-    def create_cond_block(self):
-        print("no work")
 
 class CreationZone(QScrollArea):
     def __init__(self):
@@ -96,6 +101,36 @@ class CreationZone(QScrollArea):
 
         self.layout = QGridLayout()
 
+        self.setLayout(self.layout)
+
+class ActionWidget(QFrame):
+    def __init__(self, name):
+        super().__init__()
+        self.layout = QGridLayout()
+        label = QLabel(name)
+
+        self.layout.addWidget(label)
+        self.setLayout(self.layout)
+
+
+class ObjectWidget(QFrame):
+    def __init__(self, name):
+        super().__init__()
+
+        self.layout = QGridLayout()
+        label = QLabel(name)
+
+        self.layout.addWidget(label)
+        self.setLayout(self.layout)
+
+class CondWidget(QFrame):
+    def __init__(self, name):
+        super().__init__()
+
+        self.layout = QGridLayout()
+        label = QLabel(name)
+
+        self.layout.addWidget(label)
         self.setLayout(self.layout)
 
 class Settings(QFrame):
