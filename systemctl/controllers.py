09f0709e10efc51e224fa12ad7cee7cf6b56820f
Carter Wilson <carter47w@gmail.com>
2020-03-11 09:50:25 -0400

    add methods to run commands on linux and find applications

--- systemctl/controllers.py
+++ systemctl/controllers.py
@@ -1,4 +1,8 @@
+import os
+import subprocess
 import sys
+
+from getpass import getuser
 
 # TODO: use ABCMeta to make this an interface for the other controllers?
 class SystemController:
@@ -14,8 +18,32 @@ class LinuxController:
     def __init__(self):
         print('linux')
 
+    def run(self, command):
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return process.communicate()
+
+    def continued_execute(self, cmd):
+        popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        for stdout_line in iter(popen.stdout.readline, ""):
+            yield stdout_line
+        popen.stdout.close()
+        return_code = popen.wait()
+        if return_code:
+            raise subprocess.CalledProcessError(return_code, cmd)
+
     def close(self, application):
         print('closing {} on linux'.format(str(application)))
+
+    def get_applications(self):
+        desktop_entries = []
+        for loc in ('/usr/share/applications',
+                    '/usr/local/share/applications',
+                    f'/home/{getuser()}/.local/share/applications'):
+            try:
+                desktop_entries += os.listdir(loc)
+            except FileNotFoundError:
+                pass
+        return list(map(lambda x:x.strip('.desktop'), desktop_entries))
 
 
 class MacController:
