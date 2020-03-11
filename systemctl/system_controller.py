5f9cb844ecfc520431116ca26dfe3b5383cf960e
Carter Wilson <carter47w@gmail.com>
2020-03-11 10:41:00 -0400

    refactor systemctl to one class per file

---
+++ systemctl/system_controller.py
@@ -0,0 +1,14 @@
+import sys
+
+from systemctl.linux_controller import LinuxController
+from systemctl.mac_controller import MacController
+from systemctl.windows_controller import WindowsController
+
+# TODO: use ABCMeta to make this an interface for the other controllers?
+class SystemController:
+    def __new__(cls):
+        if sys.platform == 'linux':
+            return LinuxController()
+        if sys.platform == 'darwin':
+            return MacController()
+        return WindowsController()
